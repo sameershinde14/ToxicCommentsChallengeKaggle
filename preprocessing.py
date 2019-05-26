e94a42f02ea4da51e92263bb86f82d43099c7675
Pranav Udupa S <pranavudupa94@gmail.com>
2019-05-26 14:50:16 -0700

    Added cleaning

--- preprocessing.py
+++ preprocessing.py
@@ -13,17 +13,24 @@ import nltk
 from matplotlib import pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
+from utils.data_loader import load_train_data, load_test_data
+
 # %% [markdown]
 # ## Data Exploration
 
 # %%
-train_data = pd.read_csv("data/train.csv")
-test_data = pd.read_csv("data/test.csv")
-test_labels = pd.read_csv("data/test_labels.csv")
-
+train_data, valid_data = load_train_data("data/train.csv")
+loaded_test_data = load_test_data("data/test.csv", "data/test_labels.csv")
+test_data = loaded_test_data[["id", "comment_text"]]
+test_labels = loaded_test_data[
+    [
+        "toxic", "severe_toxic", "obscene",
+        "threat", "insult", "identity_hate"
+    ]
+]
 
 # %%
-len(train_data), len(test_data)
+len(train_data), len(valid_data), len(test_data)
 
 
 # %%
@@ -55,6 +62,8 @@ classes = ["toxic", "severe_toxic", "obscene",
            "threat", "insult", "identity_hate"]
 train_y = train_data[classes].values
 train_x = train_data["comment_text"]
+valid_y = valid_data[classes].values
+valid_x = valid_data["comment_text"]
 test_x = test_data["comment_text"]
 
 
@@ -76,21 +85,23 @@ tokenizer.fit_on_texts(list(train_x))
 
 # %%
 tokenized_train_x = tokenizer.texts_to_sequences(train_x)
+tokenized_valid_x = tokenizer.texts_to_sequences(valid_x)
 tokenized_test_x = tokenizer.texts_to_sequences(test_x)
 
 
 # %%
-len(tokenized_train_x), len(tokenized_test_x)
+len(tokenized_train_x), len(tokenized_valid_x), len(tokenized_test_x)
 
 
 # %%
 maxlen = 200
 X_train = pad_sequences(tokenized_train_x, maxlen=maxlen)
+X_valid = pad_sequences(tokenized_valid_x, maxlen=maxlen)
 X_test = pad_sequences(tokenized_test_x, maxlen=maxlen)
 
 
 # %%
-len(X_train), len(X_test)
+len(X_train), len(X_valid), len(X_test)
 
 
 # %%
@@ -117,20 +128,32 @@ train_x[0]
 X_train[0]
 
 # %% [markdown]
+# ### Clean the data
+%load_ext autoreload
+%autoreload 2
+from utils.preprocessing import clean
+
+clean_train_x = clean(train_x)
+clean_valid_x = clean(valid_x)
+clean_test_x = clean(test_x)
+
+# %% [markdown]
 # ### Stopwords
 
 # %%
 %load_ext autoreload
 %autoreload 2
 from utils.preprocessing import filter_stop_words
-filtered_train_x = filter_stop_words(train_x)
-filtered_test_x = filter_stop_words(test_x)
+filtered_train_x = filter_stop_words(clean_train_x)
+filtered_valid_x = filter_stop_words(clean_valid_x)
+filtered_test_x = filter_stop_words(clean_test_x)
 
 tokenizer = Tokenizer(num_words=max_words)
 tokenizer.fit_on_texts(list(filtered_train_x))
 
 # %%
 tokenized_train_x = tokenizer.texts_to_sequences(filtered_train_x)
+tokenized_valid_x = tokenizer.texts_to_sequences(filtered_valid_x)
 tokenized_test_x = tokenizer.texts_to_sequences(filtered_test_x)
 
 # %%
@@ -150,12 +173,14 @@ plt.show()
 from utils.preprocessing import lemmatize
 
 lemmatized_train_x = lemmatize(filtered_train_x)
+lemmatized_valid_x = lemmatize(filtered_valid_x)
 lemmatized_test_x = lemmatize(filtered_test_x)
 
 tokenizer.fit_on_texts(list(lemmatized_train_x))
 
 # %%
 tokenized_train_x = tokenizer.texts_to_sequences(lemmatized_train_x)
+tokenized_valid_x = tokenizer.texts_to_sequences(lemmatized_valid_x)
 tokenized_test_x = tokenizer.texts_to_sequences(lemmatized_test_x)
 # %%
 total_num_words = [len(one_comment) for one_comment in tokenized_train_x]
@@ -172,12 +197,14 @@ plt.show()
 from utils.preprocessing import stem
 
 stemmed_train_x = stem(filtered_train_x)
+stemmed_valid_x = stem(filtered_valid_x)
 stemmed_test_x = stem(filtered_test_x)
 
 tokenizer.fit_on_texts(list(stemmed_train_x))
 
 # %%
 tokenized_train_x = tokenizer.texts_to_sequences(stemmed_train_x)
+tokenized_valid_x = tokenizer.texts_to_sequences(stemmed_valid_x)
 tokenized_test_x = tokenizer.texts_to_sequences(stemmed_test_x)
 # %%
 total_num_words = [len(one_comment) for one_comment in tokenized_train_x]
@@ -193,3 +220,9 @@ print(train_x[0], "\n")
 print(filtered_train_x[0], "\n")
 print(lemmatized_train_x[0], "\n")
 print(stemmed_train_x[0], "\n")
+
+#%%
+print(valid_x[0], "\n")
+print(filtered_valid_x[0], "\n")
+print(lemmatized_valid_x[0], "\n")
+print(stemmed_valid_x[0], "\n")
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,6 +1,12 @@
+import re
+import unicodedata
+import inflect
+
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize, sent_tokenize
+
 WORD_CHARACTER_LIMIT = 30
 
 
@@ -46,3 +52,23 @@ def stem(sentences):
     ]
 
     return stemmed_sentences
+
+def remove_non_ascii(word):
+    return unicodedata.normalize('NFKD',word).encode('ascii','ignore').decode('utf-8','ignore')
+
+def remove_punctuations(sentence):
+    return re.sub(r'[^\w\s]', '', sentence)
+
+def replace_numbers(sentence):
+    return re.sub(r'[0-9]*', '', sentence)
+
+def clean(sentences):
+    for index, sentence in enumerate(sentences):
+        sentence = replace_numbers(sentence)
+        sentence = remove_punctuations(sentence)
+        words = word_tokenize(sentence)
+        words = map(remove_non_ascii, words)
+        words = map(lambda x: x.lower(), words)
+        sentences[index] = " ".join(words)
+
+    return sentences
